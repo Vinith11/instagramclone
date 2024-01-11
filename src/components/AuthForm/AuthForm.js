695fc73905997c5794acb956e12ff0c1c5cb64ca
Vinith <vinith2003@gmail.com>
2024-01-11 06:49:18 +0530

    Refactor AuthForm component and update AuthPage layout

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,38 +1,95 @@
-import { Box, VStack, Image, Input, Button, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Image,
+  Input,
+  Button,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 import logo from "../../assets/logo.png";
 import { useState } from "react";
+import google from "../../assets/google.png";
+import { useNavigate } from "react-router-dom";
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleAuth = () =>{
+    if(!inputs.email || !inputs.password ){
+      alert("Please fill all the fields");
+      return;
+    }
+
+    navigate("/");
+  }
 
   return (
-    <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-      <VStack spacing={4}>
-        <Image src={logo} h={24} cursor={"pointer"} alt="logo image" />
-        <Input placeholder={"Email"} type="email" fontSize={14} />
-        <Input placeholder={"Password"} type="password" fontSize={14} />
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src={logo} h={24} cursor={"pointer"} alt="logo image" />
+          <Input placeholder={"Email"} value={inputs.email} onChange={(e)=> setInputs({...inputs,email:e.target.value})} type="email" fontSize={14} />
+          <Input placeholder={"Password"} value={inputs.password} onChange={(e)=> setInputs({...inputs,password:e.target.value})} type="password" fontSize={14} />
 
-        {isLogin ? (
-          <Input
-            placeholder={"Confirm password"}
-            type="password"
-            fontSize={14}
-          />
-        ) : null}
+          {isLogin ? null : (
+            <Input
+              placeholder={"Confirm password"}
+              value={inputs.confirmPassword} onChange={(e)=> setInputs({...inputs,confirmPassword:e.target.value})}
+              type="password"
+              fontSize={14}
+            />
+          )}
 
-        <Button width={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+          <Button width={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={handleAuth}>
+            {isLogin ? "Login" : "Sign up"}
+          </Button>
+
+          {/* Or part */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            my={4}
+            gap={1}
+            w={"full"}
+          >
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+            <Text mx={1} color={"white"}>
+              Or
+            </Text>
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+          </Flex>
+
+          <Flex
+            alignItems={"center"}
+            cursor={"pointer"}
+            justifyContent={"center"}
+          >
+            <Image src={google} w={5} alt="google logo" />
+            <Text mx={2} color={"blue.500"}>
+              Log in with Google
+            </Text>
+          </Flex>
+        </VStack>
+      </Box>
+
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box onClick={()=> setIsLogin(!isLogin)} color={"blue.500"}>
             {isLogin ? "Sign up" : "Login"}
-        </Button>
-
-
-            {/* Or part */}
-        <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"} >
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
-            <Text mx={1} color={"white"}>Or</Text>
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
+          </Box>
         </Flex>
-      </VStack>
-    </Box>
+      </Box>
+    </>
   );
 }
 
--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -3,31 +3,31 @@ import React from "react";
 import AuthForm from "../../components/AuthForm/AuthForm";
 import playstore from "../../assets/playstore.png"
 import microsoft from "../../assets/microsoft.png"
-import phone from "../../assets/auth.png"
+import auth from "../../assets/auth.png"
 
 function AuthPage() {
   return (
-    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
-      <Container maxW={Container.md} padding={0}>
-        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
-          {/* Left side */}
-        <Box display={{ base: "none", md: "block" }}>
-          <Image src={phone} h={650} alt="Phone image" />
-        </Box>
+		<Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+			<Container maxW={"container.md"} padding={0}>
+				<Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+					{/* Left hand-side */}
+					<Box display={{ base: "none", md: "block" }}>
+						<Image src={auth} h={650} alt='Phone img' />
+					</Box>
 
-        {/* Right side */}
-        <VStack spacing={4} align={"stretch"}>
-          <AuthForm />
-          <Box textAlign={"center"}>Get the app</Box>
-          <Flex gap={5} justifyContent={"center"}>
-            <Image src={playstore} height={"10"} alt="Logo" />
-            <Image src={microsoft} height={"10"} alt="Logo" />
-          </Flex>
-        </VStack>
-        </Flex>
-      </Container>
-    </Flex>
-  );
+					{/* Right hand-side */}
+					<VStack spacing={4} align={"stretch"}>
+						<AuthForm />
+						<Box textAlign={"center"}>Get the app.</Box>
+						<Flex gap={5} justifyContent={"center"}>
+							<Image src={playstore} h={"10"} alt='Playstore logo' />
+							<Image src={microsoft} h={"10"} alt='Microsoft logo' />
+						</Flex>
+					</VStack>
+				</Flex>
+			</Container>
+		</Flex>
+	);
 }
 
 export default AuthPage;
