2dd1b6cee7a6c7187d9df1fac767726d61923b8a
Vinith <vinith2003@gmail.com>
2024-01-21 13:36:03 +0530

    Refactor search functionality and add user search modal

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,20 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
-import React from 'react'
-import Feedposts from '../../components/Feedposts/Feedposts'
-import SuggestedUsers from '../../components/SuggestedUsers/SuggestedUsers'
+import { Box, Container, Flex } from "@chakra-ui/react";
+import FeedPosts from "../../components/Feedposts/Feedposts";
+import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
-function HomePage() {
-  return (
-    <Container maxW={"container.lg"}>
-      <Flex gap={20}>
-        <Box flex={2} py={10}>
-          <Feedposts />
-        </Box>
+const HomePage = () => {
+	return (
+		<Container maxW={"container.lg"}>
+			<Flex gap={20}>
+				<Box flex={2} py={10}>
+					<FeedPosts />
+				</Box>
+				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
+					{/* <SuggestedUsers /> */}
+				</Box>
+			</Flex>
+		</Container>
+	);
+};
 
-        <Box flex={3} mr={20}
-          display={{ base: "none", lg: "block" }}
-          maxW={"300px"}
-        >
-          <SuggestedUsers/>
-        </Box>
-      </Flex>
-
-    </Container>
-  )
-}
-
-export default HomePage
+export default HomePage;
