dd315bdddbf0e7b24f76a2221356f61a41807d1f
Vinith <vinith2003@gmail.com>
2024-01-21 17:17:09 +0530

    Update Firebase configuration with environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,26 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtLZUeE-Sd3eWQXIb7Dk_ge8VFFltVGb0",
-  authDomain: "instaclone-ffb59.firebaseapp.com",
-  projectId: "instaclone-ffb59",
-  storageBucket: "instaclone-ffb59.appspot.com",
-  messagingSenderId: "921637534203",
-  appId: "1:921637534203:web:567a5394a41da1135db35e",
-  measurementId: "G-344VSTNHPL",
+  // apiKey: "AIzaSyDtLZUeE-Sd3eWQXIb7Dk_ge8VFFltVGb0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+
+  // authDomain: "instaclone-ffb59.firebaseapp.com",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+  // projectId: "instaclone-ffb59",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
+  // storageBucket: "instaclone-ffb59.appspot.com",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+  // messagingSenderId: "921637534203",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+
+  // appId: "1:921637534203:web:567a5394a41da1135db35e",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
+  // measurementId: "G-344VSTNHPL",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
