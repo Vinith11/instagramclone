1335be9e2018f1f4956b26163249442b7619295c
Vinith <vinith2003@gmail.com>
2024-01-28 18:22:25 +0530

    Fix user's full name in SuggestedUser component

--- src/components/SuggestedUsers/SuggestedUsers.js
+++ src/components/SuggestedUsers/SuggestedUsers.js
@@ -1,36 +1,41 @@
-import { Box, Flex, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
-import SuggestedUser from './SuggestedUser'
-import SuggestedHeader from './SuggestedHeader'
-import img1 from "../../assets/img1.png";
-import img2 from "../../assets/img2.png";
-import img3 from "../../assets/img3.png";
-import img4 from "../../assets/img4.png";
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
+import SuggestedHeader from "./SuggestedHeader";
+import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
-function SuggestedUsers() {
-  return (
-    <VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
-        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-            <Text fontSize={12} fontWeight={"bold"} color= {"gary.500"}>
-                Suggested for you
-            </Text>
-            <Text fontSize={12} fontWeight={"bold"} _hover={{color: "gary.400"}} cursor={"pointer"}>
-                See All
-            </Text>
-        </Flex>
+const SuggestedUsers = () => {
+	const { isLoading, suggestedUsers } = useGetSuggestedUsers();
 
-        <SuggestedUser followers={690} name="aa" avatar={img1}/>
-        <SuggestedUser followers={690} name="bb" avatar={img2}/>
-        <SuggestedUser followers={690} name="cc" avatar={img3}/>
-        <SuggestedUser followers={690} name="dd" avatar={img4}/>
+	// optional: render loading skeleton
+	if (isLoading) return null;
 
-        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-    Libk to wef
-        </Box>
+	return (
+		<VStack py={8} px={6} gap={4}>
+			<SuggestedHeader />
 
-    </VStack>
-  )
-}
+			{suggestedUsers.length !== 0 && (
+				<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+					<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+						Suggested for you
+					</Text>
+					<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+						See All
+					</Text>
+				</Flex>
+			)}
 
-export default SuggestedUsers
+			{suggestedUsers.map((user) => (
+				<SuggestedUser user={user} key={user.id} />
+			))}
+
+			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				© 2023 Built By{" "}
+				<Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
+					As a Programmer
+				</Link>
+			</Box>
+		</VStack>
+	);
+};
+
+export default SuggestedUsers;
--- src/hooks/useFollowUser.js
+++ src/hooks/useFollowUser.js
@@ -5,7 +5,6 @@ import useShowToast from "./useShowToast";
 import { doc, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 
-
 const useFollowUser = (userId) => {
   // since we doing async stuff, we need to use useState
   const [isUpdating, setIsUpdating] = React.useState(false);
@@ -18,56 +17,69 @@ const useFollowUser = (userId) => {
   const handleFollowUser = async () => {
     setIsUpdating(true);
     try {
-        const currentUserRef = doc(firestore, "users", authUser.uid);
-        const userToFollowOrUnfollowRef = doc(firestore, "users", userId);
+      const currentUserRef = doc(firestore, "users", authUser.uid);
+      const userToFollowOrUnfollowRef = doc(firestore, "users", userId);
 
-        await updateDoc(currentUserRef, {
-            following: isFollowing ? arrayRemove(userId) : arrayUnion(userId)
-        })
+      await updateDoc(currentUserRef, {
+        following: isFollowing ? arrayRemove(userId) : arrayUnion(userId),
+      });
 
-        await updateDoc(userToFollowOrUnfollowRef, {
-            followers: isFollowing ? arrayRemove(authUser.uid) : arrayUnion(authUser.uid)
-        })
+      await updateDoc(userToFollowOrUnfollowRef, {
+        followers: isFollowing
+          ? arrayRemove(authUser.uid)
+          : arrayUnion(authUser.uid),
+      });
 
-        if(isFollowing){
-            //unfollow
-            setAuthUser({
-                ...authUser,
-                following: authUser.following.filter((uid) => uid !== userId)
-            })
-            setUserProfile({
-                ...userProfile,
-                followers: userProfile.followers.filter((uid) => uid !== authUser.uid)
-            })
-
-            localStorage.setItem("user-info", JSON.stringify({
-                ...authUser,
-                following: authUser.following.filter((uid) => uid !== userId)
-            }));
-
-            setIsFollowing(false);
-        }
-        else{
-            //follow
-            setAuthUser({
-                ...authUser,
-                following: [...authUser.following, userId]
-            })
-            setUserProfile({
-                ...userProfile,
-                followers: [...userProfile.followers, authUser.uid]
-            })
-            localStorage.setItem("user-info", JSON.stringify({
-                ...authUser,
-                following: [...authUser.following, userId]
-            }));
-            setIsFollowing(true);
+      if (isFollowing) {
+        //unfollow
+        setAuthUser({
+          ...authUser,
+          following: authUser.following.filter((uid) => uid !== userId),
+        });
+        if (userProfile) {
+          setUserProfile({
+            ...userProfile,
+            followers: userProfile.followers.filter(
+              (uid) => uid !== authUser.uid
+            ),
+          });
         }
 
+        localStorage.setItem(
+          "user-info",
+          JSON.stringify({
+            ...authUser,
+            following: authUser.following.filter((uid) => uid !== userId),
+          })
+        );
+
+        setIsFollowing(false);
+      } else {
+        //follow
+        setAuthUser({
+          ...authUser,
+          following: [...authUser.following, userId],
+        });
+        if (userProfile) {
+          setUserProfile({
+            ...userProfile,
+            followers: [...userProfile.followers, authUser.uid],
+          });
+        }
+
+        localStorage.setItem(
+          "user-info",
+          JSON.stringify({
+            ...authUser,
+            following: [...authUser.following, userId],
+          })
+        );
+        setIsFollowing(true);
+      }
     } catch (error) {
-        showToast("Error",error.message, "error");
+      showToast("Error", error.message, "error");
     } finally {
-        setIsUpdating(false);
+      setIsUpdating(false);
     }
   };
 
